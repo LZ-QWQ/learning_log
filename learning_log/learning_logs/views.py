f030d8d4852218dc403586adda8cd1e862ccc2bd
LZ_QWQ <lz1830837889@163.com>
2019-03-07 22:45:10 +0800

    3.7

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -5,6 +5,12 @@ def index(request):
     return render(request,'learning_logs/index.html')
 
 def topics(request):
-    topics=Topic.objects.ordet_by('date_added')
+    topics=Topic.objects.order_by('date_added')
     context={'topics':topics}
     return render(request,'learning_logs/topics.html',context)
+
+def topic(request,topic_id):
+    topic=Topic.objects.get(id=topic_id)
+    entries=ttopic.entry_set.order_by('-date_added')
+    context={'topic':topic,'entries':entries}
+    return render(request,'learning_logs/topic.html',context)
